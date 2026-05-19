a71432af992b93c3f80128a963902ccfc64b4c2d
Kamal-deen2 <kamal-deen2@users.noreply.github.com>
2026-05-19 02:48:38 -0700

    deen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Truck, ArrowRight, Star, Shield, DollarSign, Clock } from 'lucide-react';
 import api from '../utils/axios';
